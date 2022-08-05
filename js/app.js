e64a844b44ec7b6b6620a93f8126f45f3bb31dc6
Lucaponz91 <lucapo1991@msn.com>
2022-08-05 16:45:44 +0200

    add milestone 2

--- js/app.js
+++ js/app.js
@@ -67,11 +67,45 @@ const posts = [
     }
 ];
 
-let postsList = document.querySelector('.posts_list')
-// posts.forEach((content) => {
+let postsList = document.querySelector('.posts-list')
+for (let i = 0; i < posts.length; i++) {
     let post = document.createElement('div')
     post.classList.add('post')
+    console.log(post)
     postsList.append(post)
-// }
-// )
+    post.innerHTML += `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${posts[i].author.image} alt="${posts[i].author.name}">
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[i].author.name}</div>
+                        <div class="post-meta__time">${posts[i].created}</div>
+                    </div>
+                </div>
+            </div>
+            <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad
+                ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+            <div class="post__image">
+                <img src=${posts[i].media} alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${posts[i].likes}</b> persone
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
+
 
